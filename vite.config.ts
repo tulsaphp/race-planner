23824a0c4125547fd957ac2c3a3c2b72f52b59ff
Tulsa gangdev <tulsa.php@gmail.com>
2026-08-03 08:20:19 +0530

    Fix setupFiles path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/race-planner/",
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/SetupTest.ts"],
+    setupFiles: ["./src/setupTests.ts"],
   },
 });
